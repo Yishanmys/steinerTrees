9b362d0dfafc6bce0e2f14885067adc512ba6a07
Christian Witte <chr_witte@yahoo.de>
2016-03-22 12:24:19 +0100

    data structure for disjoint sets

---
+++ Project/src/main/java/util/NodeSet.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author cwitte
+ */
+
+class NodeSetElement {
+    public int value;
+    
+    public NodeSetElement nextRef;
+    
+    public NodeSet setRef;
+    
+    public NodeSetElement(int value) {
+        this.value = value;
+    }
+}
+
+
+
+
+public class NodeSet {
+    private int size;
+    
+    private NodeSetElement firstElement;
+    
+    private NodeSetElement lastElement;
+    
+    /**
+     * Creates a new set containing only the given element.
+     * @param element Element to be added to the new set
+     */
+    public NodeSet(NodeSetElement element) {
+        size = 1;
+        firstElement = element;
+        element.setRef = this;
+        lastElement = null;
+    }
+    
+    
+    
+    /**
+     * Adds the elements of the smaller set to bigger one and returns the modified set.
+     * @param set1 First set
+     * @param set2 Second set
+     * @return Union set of the two given sets
+     */
+    public static NodeSet union(NodeSet set1, NodeSet set2) {
+        NodeSet smallerSet, biggerSet;
+        
+        if(set1.size <= set2.size) {
+            smallerSet = set1;
+            biggerSet = set2;
+        } else {
+            smallerSet = set2;
+            biggerSet = set1;
+        }
+        
+        biggerSet.lastElement.nextRef = smallerSet.firstElement;
+        biggerSet.lastElement = smallerSet.lastElement;
+        biggerSet.size += smallerSet.size;
+        
+        NodeSetElement element = smallerSet.firstElement;
+        while(element.nextRef != null) {
+            element.setRef = biggerSet;
+            element = element.nextRef;
+        }
+        
+        return biggerSet;
+    }
+}
